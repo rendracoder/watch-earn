f21c979f5010d81f02f3a7545e01c4e24ac5991f
Project343 <35072818+rendracoder@users.noreply.github.com>
2019-04-03 23:04:54 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+//ganti ticket & meid dengan punya mu!!
+//jangan ada spasi bro
+//jika ingin menambahkan config ikuti kaya contoh dibawah
+$config = [
+
+"ticket=ticket_mu&meid=meid_mu",
+"ticket=ticket_mu&meid=meid_mu",
+"ticket=ticket_mu&meid=meid_mu",
+
+];
+
+?>
